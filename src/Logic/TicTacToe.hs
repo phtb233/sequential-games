f4493b975248dff375b6e4dd13c16c16c3171720
phtb233 <s3501316+phtb233@users.noreply.github.com>
2023-07-25 18:36:18 +0100

    further

--- src/Logic/TicTacToe.hs
+++ src/Logic/TicTacToe.hs
@@ -17,7 +17,7 @@ pPar :: [Move] -> [Move] -> R
 pPar preceding ms = value (outcome X (preceding ++ ms) ([], []))
 
 parEpsilons :: [Move] -> [[Move] -> GL.J R Move]
-parEpsilons preceding = take (9 - (length preceding)) all'
+parEpsilons preceding = take (9 - length preceding) all'
     where all = epsilonX : epsilonO : all
           all' = drop (length preceding) all
           epsilonX h = GL.argsup (poolOfMoves `GL.setMinus` h)
@@ -25,7 +25,7 @@ parEpsilons preceding = take (9 - (length preceding)) all'
           poolOfMoves = [0..8] \\ preceding
 
 parOptimalPlay :: [Move] -> [Move]
-parOptimalPlay moves = moves ++ (optimalStrategy moves)
+parOptimalPlay moves = moves ++ optimalStrategy moves
 
 value :: Board -> R
 value (x, o) | wins x    = 1
@@ -40,9 +40,9 @@ outcome X (m : ms) (x, o) =
 outcome O (m :ms) (x, o) =
     if wins x then (x, o)
     else outcome X ms (x, GL.insert m o)
-    
+
 wins :: [Move] -> Bool
-wins = 
+wins =
     GL.someContained [[0,1,2],[3,4,5],[6,7,8],
                 [0,3,6],[1,4,7],[2,5,8],
                 [0,4,8],[2,4,6]]
@@ -63,7 +63,7 @@ optimalStrategy :: [Move] -> [Move]
 optimalStrategy moves = GL.bigotimes (parEpsilons moves) (pPar moves)
 
 nextMove :: [Move] -> Move
-nextMove moves = 
+nextMove moves =
     let possibleNextMoves = map (:[]) $ [0..8] \\ moves
         possiblePlays = map (moves ++) possibleNextMoves
         results = parMap rdeepseq parOptimalPlay possiblePlays
@@ -78,18 +78,18 @@ nextMove moves =
 -- Turn based match with the computer.
 -- TODO: display tictactoe grid.
 playMatch :: IO ()
-playMatch = 
+playMatch =
     do
     let getMove :: [Move] -> IO Move
         getMove history = do
             putStrLn ( "history = " ++ show history)
             putStrLn "What is your next move?"
             input <- fmap reads getLine :: IO [(Int, String)]
-            if null input 
+            if null input
                 then putStrLn "Invalid input" >> getMove history
                 else let move = fst $ head input
                      in if move `elem` history
-                           then putStrLn "This move is taken." >> 
+                           then putStrLn "This move is taken." >>
                                   getMove history
                            else  if move < 0 || move > 8
                                      then putStrLn "Must be within 0 and 8." >>
@@ -100,7 +100,7 @@ playMatch =
             | value board == 1 = putStrLn "Player X wins"
             | value board == -1 = putStrLn "Player O wins"
             | length history == 9 = putStrLn "Its a draw"
-            | otherwise = if player == X 
+            | otherwise = if player == X
                             then do
                                  next <- fmap (:[]) $ getMove history
                                  let board' = outcome X next board
--- src/Utils.hs
+++ src/Utils.hs
@@ -2,8 +2,10 @@
 module Utils where
 
 import Data.String.Interpolate (i)
-import Control.Monad.Logger (logInfo)
+import Control.Monad.Logger (logInfo, MonadLogger)
 import Data.Text (pack)
+import GHC.Show
 
 
+logInfoHelper :: (MonadLogger m, Show a, Show b) => a -> b -> m ()
 logInfoHelper functionName message = $(logInfo) (pack [i|[#{functionName}]: #{message}|])
